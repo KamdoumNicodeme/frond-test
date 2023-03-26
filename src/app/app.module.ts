6f78848a0de5a2fd9ba5c20066c77733a50a2599
Nicodeme K <tchietcheujean@yahoo.fr>
2023-03-26 09:16:17 +0100

    add all component utils

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RegisterComponent,
+    LoginComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule
